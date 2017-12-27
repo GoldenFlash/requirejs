263c5cd5baf9c7d4095108cc34885463f01b099b
wangwei <892458829@qq.com>
2017-12-27 17:17:34 +0800

    add

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 	requirejs.config({
-    		baseUrl:'./js',
+    		baseUrl:'./js',//加载模块的基础路径
    			paths: {
         		'jquery': 'lib/jquery'
     		}
 		});
-	requirejs(['lib/tab'],function(tab,){
+	requirejs(['jquery','lib/tab'],function($,tab){
 		var tabcart = document.querySelector('.tab')
 		var cont = document.querySelector('.content')
 		tab.init(tabcart,cont)
